a735c0acb47cf2cb9657d70994595eb8fb3e0d55
JustinLGates <papagates1@gmail.com>
2020-07-31 17:09:10 -0600

    initalize

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class NavigationBar extends Component {
+  // state = {  }
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <button>Submit..</button>
+      </div>
+    );
+  }
+}
+
+export default NavigationBar;
